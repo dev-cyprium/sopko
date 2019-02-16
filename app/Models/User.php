a32f3ac4b387545b15c1023f44288a5ce5d78865
Stefan Kupresak <stefan_vg@hotmail.com>
2019-02-16 02:36:42 +0100

    initlaize git project

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+
+class User
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'company_name', 'password_hash'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password_hash'
+    ];
+}
