676e2dcd653cd5174bbb4066964d10c510292a94
Stefan Kupresak <stefan_vg@hotmail.com>
2019-02-17 12:07:40 +0100

    add migration for the storages table

---
+++ database/migrations/2019_02_17_105943_create_storage_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStorageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('type_id');
+            $table->double('geo_lat')->nullable();
+            $table->double('geo_lon')->nullable();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storages');
+    }
+}
