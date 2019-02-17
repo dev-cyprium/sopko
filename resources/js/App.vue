375482b248a9cf8f41f1b86f2d5dd06ff1a209a8
Stefan Kupresak <stefan_vg@hotmail.com>
2019-02-17 01:19:13 +0100

    Complete Vuejs setup for the application

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <div class="mx-2 my-2">
+            <h1>Vue</h1>
+            <h2>Vuetify</h2>
+            <h3>setup successfull</h3>
+        </div>
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
