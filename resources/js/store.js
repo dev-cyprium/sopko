b1d0258f07b652ced453038a6c9d0d4b843cfb48
Stefan Kupresak <stefan_vg@hotmail.com>
2019-02-21 21:05:33 +0100

    finish the login form

--- resources/js/store.js
+++ resources/js/store.js
@@ -27,7 +27,7 @@ export default new Vuex.Store({
         login({commit}, user) {
             return new Promise((resolve, reject) => {
                 commit('auth_request')
-                axios({url: '/login', data: user, method: 'POST'})
+                axios({url: '/api/login', data: user, method: 'POST'})
                 .then(resp => {
                     const {token, user} = resp.data
                     localStorage.setItem('token', token)
