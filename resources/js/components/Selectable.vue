9e244ef05f1bd4565ee262ab7879648ae6bfa84e
Stefan Kupresak <stefan_vg@hotmail.com>
2019-03-24 16:05:47 +0100

    add selectable images

---
+++ resources/js/components/Selectable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="parent-overlay" @click="onClick">
+      <div :class="selected ? 'overlay':''" />
+      <slot />
+  </div>
+</template>
+
+<style>
+    .parent-overlay {
+        position: relative;
+    }
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #4caf50;
+        z-index: 1;
+        opacity: 0.5;
+    }
+</style>
+
+
+<script>
+export default {
+    data() {
+        return {
+            selected: false
+        }
+    },
+    methods: {
+        onClick() {
+            this.selected = !this.selected
+            this.$emit('click', this.selected)
+        }
+    }
+}
+</script>
--- resources/js/views/Images.vue
+++ resources/js/views/Images.vue
@@ -8,7 +8,8 @@
                         <h3 class="headline">Upravljanje slikama proizvoda</h3>
                         <v-spacer />
                         <div class="sp-d-flex justify-content-end">
-                            <v-btn @click="dialog = true" class="skip-flex primary">Nova Slika</v-btn>
+                            <v-btn v-if="selectedImages.length == 0" @click="dialog = true" class="skip-flex primary">Nova Slika</v-btn>
+                            <v-btn v-else class="skip-flex error" >Obriši {{ selectedImages.length }}</v-btn>
                         </div>
                     </v-card-title>
                     <v-container fluid>
@@ -20,23 +21,24 @@
                             d-flex
                             >
                             <v-card flat tile class="d-flex py-1 px-1">
-                                <v-img
-                                :src="image.path"
-                                :lazy-src="image.path"
-                                
-                                class="grey lighten-2"
-                                >
-                                <template v-slot:placeholder>
-                                    <v-layout
-                                    fill-height
-                                    align-center
-                                    justify-center
-                                    ma-0
+                                <Selectable @click="itemClicked($event, image.path)">
+                                    <v-img
+                                    :src="image.path"
+                                    :lazy-src="image.path"
+                                    class="grey lighten-2 h100"
                                     >
-                                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                                    </v-layout>
-                                </template>
-                                </v-img>
+                                    <template v-slot:placeholder>
+                                        <v-layout
+                                        fill-height
+                                        align-center
+                                        justify-center
+                                        ma-0
+                                        >
+                                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                        </v-layout>
+                                    </template>
+                                    </v-img>
+                                </Selectable>
                             </v-card>
                             </v-flex>
                         </v-layout>
@@ -84,20 +86,29 @@
         min-height: 15rem;
         border: 1px dashed gray;
     }
+
+    .h100 {
+        height: 100%;
+    }
 </style>
 
 
 <script>
-import FileUpload from '../components/FileUpload';
+import FileUpload from '../components/FileUpload'
+import Selectable from '../components/Selectable'
 
 export default {
-    components: {FileUpload},
+    components: {
+        FileUpload,
+        Selectable,
+    },
     created() {
         this.$store.dispatch('images')
     },
     data() {
         return {
-            dialog: false
+            dialog: false,
+            selectedImages: []
         }
     },
     computed: {
@@ -107,6 +118,13 @@ export default {
         handleDodaj() {
             this.dialog = false
             this.$refs.fileupload.doFileUpload()
+        },
+        itemClicked(selected, path) {
+            if(selected) {
+                this.selectedImages.push(path)
+            } else {
+                this.selectedImages = this.selectedImages.filter(item => item != path)
+            } 
         }
     }
 }
